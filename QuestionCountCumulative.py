11d40dc613a75a2c4a70062698011e7b5b178e25
Diego Rodriguez <diegoarodriguez741@gmail.com>
2018-02-12 13:29:39 -0500

    Question Cumulative added and Talk Ratio fixed

--- QuestionCountCumulative.py
+++ QuestionCountCumulative.py
@@ -11,8 +11,7 @@ import numpy as np
 
 SAMPLE = { 
 			'x' : ['12/1', '12/3', '12/5', '12/7'],
-			'ta_talk_perc' : [.343434, .343434, .129032, .281250],
-			'st_talk_perc' : [.656566, .656566, .870968, .718750]
+			'y' : [20, 2, 2, 2]
 		}
 
 def plot_talk_ratio(data):
@@ -20,21 +19,21 @@ def plot_talk_ratio(data):
 
 	plot = bf(tools = "xpan", x_range = source.data["x"])
 
-	plot.title.text = 'Talk Ratio'
+	plot.title.text = 'Rate of Questions'
 	plot.title.align = 'center'
 	plot.title.text_font_size = '24px'
 
 	plot.xaxis.axis_label = 'Date'
-	plot.y_range = Range1d(0, 1)
-	plot.yaxis.formatter = NumeralTickFormatter(format="0.0%")
+	plot.yaxis.axis_label = 'Questions per Minutes'
+	plot.xaxis.minor_tick_line_color = None
+	plot.yaxis.minor_tick_line_color = None
+	plot.y_range = Range1d(0, max(int(max(data['y']) * 1.5), 10))
+	plot.xgrid.grid_line_color = None
 
-	plot.vbar(x='x', bottom=0, top='ta_talk_perc', width=0.5, source=source, color = '#3FA5E2', legend = 'Student')
-	plot.vbar(x='x', bottom='ta_talk_perc', top=1, width=0.5, source=source, color = '#E2793F', legend = 'TA')
-	goal_line = Span(location=.2, dimension='width', line_color='#3FE258', line_width=2, line_dash = "dashed")
+	plot.vbar(x='x', bottom = 0, top = 'y',width=0.5, source=source, color = '#3FA5E2')
+	goal_line = Span(location=5, dimension='width', line_color='#3FE258', line_width=2, line_dash = "dashed")
 
 	plot.renderers.extend([goal_line])
-	gridplot([[plot]], sizing_mode='scale_width')
-	file_html(plot, CDN, "talk_ratio")
 	show(plot)
 
 plot_talk_ratio(SAMPLE)
