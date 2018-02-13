6e134614a406e61ce26e48265b12d8b3f4f223d4
Diego Rodriguez <diegoarodriguez741@gmail.com>
2018-02-12 21:06:52 -0500

    Added date stamp to Talk ratio

--- TalkRatioBokeh.py
+++ TalkRatioBokeh.py
@@ -1,6 +1,6 @@
 from bokeh.plotting import figure, output_file, show, figure as bf
 from bokeh.models import ColumnDataSource, NumeralTickFormatter, TickFormatter
-from bokeh.models import Span, DatetimeTickFormatter, Range1d, Legend, LegendItem
+from bokeh.models import Span, DatetimeTickFormatter, Range1d, Label, Title
 from bokeh.sampledata.autompg import autompg as df
 from bokeh.resources import CDN
 from bokeh.embed import file_html
@@ -15,7 +15,7 @@ SAMPLE = {
 			'st_talk_perc' : [.656566, .656566, .870968, .718750]
 		}
 
-def plot_talk_ratio(data):
+def plot_talk_ratio(data, date):
 	source = ColumnDataSource(data=data)
 
 	plot = bf(tools = "xpan", x_range = source.data["x"])
@@ -28,14 +28,17 @@ def plot_talk_ratio(data):
 	plot.y_range = Range1d(0, 1)
 	plot.yaxis.formatter = NumeralTickFormatter(format="0.0%")
 
-	st = plot.vbar(x='x', bottom=0, top='ta_talk_perc', width=0.5,
+	plot.vbar(x='x', bottom=0, top='ta_talk_perc', width=0.5,
 				   source=source, color = '#3FA5E2', legend = 'Student')
-	ta = plot.vbar(x='x', bottom='ta_talk_perc', top=1, width=0.5,
+	plot.vbar(x='x', bottom='ta_talk_perc', top=1, width=0.5,
 				   source=source, color = '#E2793F', legend = 'TA')	
-	goal = plot.ray(x=0, y=.2, length=0, angle = 0, line_width = 2,
+	plot.ray(x=0, y=.2, length=0, angle = 0, line_width = 2,
 					line_color = "#3FE258", line_dash = "dashed", legend = "Goal")
-	gridplot([[plot]], sizing_mode='scale_width')
-	file_html(plot, CDN, "talk_ratio")
+	plot.add_layout(Title(text="Data from %s" % date, align = "left", text_font_size = "12px",
+					text_font_style = "italic", text_alpha = .5), "below")
+	# plot.add_layout(Label(text ="Howdy"), "below")
+	# gridplot([[plot]], sizing_mode='scale_width')
+	# file_html(plot, CDN, "talk_ratio")
 	show(plot)
-plot_talk_ratio(SAMPLE)
+plot_talk_ratio(SAMPLE, "12/07/2018")
 
