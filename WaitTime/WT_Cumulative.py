4dfab3765f42b49af2df76ce2bc16d1982aab58d
Alan Hodge <alanhodg@andrew.cmu.edu>
2018-03-23 17:38:59 -0400

    final fixes hopefully

--- WaitTime/WT_Cumulative.py
+++ WaitTime/WT_Cumulative.py
@@ -1,21 +1,30 @@
 from bokeh.plotting import figure, output_file, show
 from bokeh.models import ColumnDataSource, Title, Range1d, BoxAnnotation
 
-recitation_dates = ["12/1", "12/8", "12/15", "12/22"]
+#recitation_dates = ("12/1", "12/8", "12/15", "12/22")
 #wait times for box plot
-mins = [1,None, 2, 1]
-first_quart = [2, 3, 4, 2]
-medians = [5, None, 6, 3]
-third_quart = [6, 5, 8, 5]
-maxs = [9, None, 10, 7]
+#mins = (1,None, 2, 1)
+#first_quart =(2, 3, 4, 2)
+#medians = (5, None, 6, 3)
+#third_quart = (6, 5, 8, 5)
+#maxs = (9, None, 10, 7)
 
-#output_file("WT_Cumulative.html")
+data = dict(
+mins=[1,None, 2, 1], 
+first_quart=[2, 3, 4, 2],
+medians=[5, None, 6, 3],
+third_quart=[6, 5, 8, 5],
+maxs=[9, None, 10, 7],
+recitation_dates=["12/1", "12/8", "12/15", "12/22"]
+)
+
 
 def main(data):
+    source = ColumnDataSource(data=data)
     y_axis_range = (0,10)
-    if max(filter(None, maxs)) >= 10: 
+    if max(filter(None, source.data['maxs'])) >= 10: 
         y_axis_range = (0,11)
-    p = figure(plot_width=325, plot_height=325,x_range=recitation_dates, y_range=y_axis_range)
+    p = figure(plot_width=325, plot_height=325,x_range=source.data['recitation_dates'], y_range=y_axis_range)
     p.add_layout(Title(text="Wait Time Multiple Days", align="center", text_font_size="14pt",
                           text_font_style="bold", text_alpha=1), "above")
                           
@@ -27,25 +36,25 @@ def main(data):
     p.yaxis.axis_label = "Wait Time (seconds)"
     
     #goal line    
-    p.line([0,len(recitation_dates)],[3,3], line_color="#3FE258", line_dash=[4,4], line_width=3)
+    p.line([0,len(source.data['recitation_dates'])],[3,3], line_color="#3FE258", line_dash=[4,4], line_width=3)
     line_w = 1
     
-    for i in range(len(recitation_dates)):
-        if medians[i] == None:
+    for i in range(len(source.data['recitation_dates'])):
+        if source.data['medians'][i] == None:
             continue 
         #horizontal boxplot lines
-        p.line(x=[i+.375,i+.625], y=[mins[i],mins[i]], line_color="black", line_width=line_w)
-        p.line(x=[i+.25,i+.75], y=[first_quart[i],first_quart[i]], line_color="black", line_width=line_w)
-        p.line(x=[i+.25,i+.75], y=[medians[i],medians[i]], line_color="black", line_width=line_w+1, 
-                legend=value("Median of Day"))
-        p.line(x=[i+.25,i+.75], y=[third_quart[i],third_quart[i]], line_color="black", line_width=line_w)
-        p.line(x=[i+.375,i+.625], y=[maxs[i],maxs[i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.375,i+.625], y=[source.data['mins'][i],source.data['mins'][i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.25,i+.75], y=[source.data['first_quart'][i],source.data['first_quart'][i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.25,i+.75], y=[source.data['medians'][i],source.data['medians'][i]], line_color="black", line_width=line_w+1, 
+                legend=("Median of Day"))
+        p.line(x=[i+.25,i+.75], y=[source.data['third_quart'][i],source.data['third_quart'][i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.375,i+.625], y=[source.data['maxs'][i],source.data['maxs'][i]], line_color="black", line_width=line_w)
         
         #vertical boxplot lines
-        p.line(x=[i+.25,i+.25], y=[first_quart[i],third_quart[i]], line_color="black", line_width=line_w)
-        p.line(x=[i+.75,i+.75], y=[first_quart[i],third_quart[i]], line_color="black", line_width=line_w)
-        p.line(x=[i+.5,i+.5], y=[first_quart[i],mins[i]], line_color="black", line_width=line_w)
-        p.line(x=[i+.5,i+.5], y=[third_quart[i],maxs[i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.25,i+.25], y=[source.data['first_quart'][i],source.data['third_quart'][i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.75,i+.75], y=[source.data['first_quart'][i],source.data['third_quart'][i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.5,i+.5], y=[source.data['first_quart'][i],source.data['mins'][i]], line_color="black", line_width=line_w)
+        p.line(x=[i+.5,i+.5], y=[source.data['third_quart'][i],source.data['maxs'][i]], line_color="black", line_width=line_w)
 
     #style
     p.title.text_font_size = '18pt'
@@ -69,5 +78,4 @@ def main(data):
 
     show(p)
     
-data = [recitation_dates, medians]
 main(data)
--- WaitTime/WT_Daily.py
+++ WaitTime/WT_Daily.py
@@ -1,20 +1,22 @@
 from bokeh.plotting import figure, show
-from bokeh.models import Title, Range1d, BoxAnnotation
+from bokeh.models import ColumnDataSource, Title, Range1d, BoxAnnotation
 import numpy as np
 from bokeh.core.properties import value
 
-# output_file("WTDaily1.html")
 
 # Data Set (Variable inputs)
-class_duration = 80  # how long class was (in minutes)
-x = [8, 80]  # when questions were asked
-y = [5,7]  # wait times
-date = "12/07/2018"
+data = dict(
+class_duration =[80],  # how long class was (in minutes)
+x = [8, 80],  # when questions were asked
+y = [5,7],  # wait times
+date = ["12/07/2018"]
+)
 
 # Figure
 def main(data):
+    source = ColumnDataSource(data=data)
     y_axis_range = Range1d(0,10)
-    if max(filter(None, y)) >= 10: 
+    if max(filter(None, source.data['y'])) >= 10: 
         y_axis_range = Range1d(0,12)
     p = figure(plot_width=325, plot_height=325)
     p.add_layout(Title(text="Wait Time After Question", align="center", text_font_size="14pt",
@@ -26,16 +28,29 @@ def main(data):
     p.add_layout(BoxAnnotation(bottom=3, top=12, fill_alpha=0.1, fill_color='#3FE258'))  # GoodWT
     p.add_layout(BoxAnnotation(top=3, fill_alpha=0.1, fill_color='#E2793F'))  # BadWT
 
-    p.line([0, class_duration+2], [3, 3], line_color='#3FE258', line_width=2, line_dash=[4, 4])  # ideal line
+    p.line([0, source.data['class_duration'][0]+2], [3, 3], line_color='#3FE258', line_width=2, line_dash=[4, 4])  # ideal line
 
-    p.circle(x, y, size=10, color="black", alpha=1, legend=value("Content Question"))
+    p.circle(source.data['x'], source.data['y'], size=10, color="black", alpha=1, legend=("Content Question"))
     p.grid.grid_line_color = None
     p.background_fill_color = "white"
     p.y_range = y_axis_range
-    p.x_range = Range1d(0, class_duration+2)
+    p.x_range = Range1d(0, source.data['class_duration'][0]+2)
     p.xaxis.axis_label = "Class Duration (minutes)"
     p.yaxis.axis_label = "Wait Time (seconds)"
 
+    # Date
+    p.add_layout(Title(text="Data from %s" % source.data['date'][0], align="left", text_font_size="12px",
+                          text_font_style="italic", text_alpha=.5), "below")
+
+    if source.data['y']:
+        WT_median = np.median(source.data['y'])
+        if WT_median >= 3:
+            WT_median_color = "#3FE258"
+        else:
+            WT_median_color = "#E2793F"
+
+        p.line([0, source.data['class_duration'][0]+2], [WT_median, WT_median],
+               line_color=WT_median_color, line_width=2, legend=value("Median Wait Time"))  # median line
     # style
     p.title.text_font_size = '18pt'
     p.title.align = 'center'
@@ -56,23 +71,6 @@ def main(data):
     p.legend.label_text_font_size = '6pt'
     p.legend.background_fill_alpha = 0.8
     
-    # Date
-    p.add_layout(Title(text="Data from %s" % date, align="left", text_font_size="12px",
-                          text_font_style="italic", text_alpha=.5), "below")
-
-    if y:
-        WT_median = np.median(y)
-        if WT_median >= 3:
-            WT_median_color = "#3FE258"
-        else:
-            WT_median_color = "#E2793F"
-
-        p.line([0, class_duration+2], [WT_median, WT_median],
-               line_color=WT_median_color, line_width=2, legend=value("Median Wait Time"))  # median line
-
-
     show(p)
 
-
-data = [x, y, class_duration, date]
 main(data)
